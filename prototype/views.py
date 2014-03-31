abfa6bf53533c80e5d98d95ed2df55829ef09954
Gary Reynolds <gary@touch.asn.au>
2014-03-31 17:58:06 +1100

    Simple application that allows for quick prototypes based on django templates to be constructed.

---
+++ prototype/views.py
@@ -0,0 +1,15 @@
+import os
+
+from django.views.generic import TemplateView
+
+
+class PrototypeView(TemplateView):
+    """
+    Simple ``TemplateView`` subclass that will determine the template path
+    dynamically based upon it's urlconf.
+    """
+    def get_template_names(self):
+        return os.path.join('prototype', self.kwargs['path'], 'index.html')
+
+
+prototype = PrototypeView.as_view()
