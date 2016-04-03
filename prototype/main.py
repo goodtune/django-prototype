2da64cb3d650cdc21a5cd7eabbc231cfb26cb590
sirex <sirexas@gmail.com>
2016-04-03 14:03:27 +0300

    Fix prototype.yml reload
    When prototype.yml is changed, whole server have to be realoaded, because
    urls.py is generated based on prototype.yml.

--- prototype/main.py
+++ prototype/main.py
@@ -1,4 +1,9 @@
 import os
+import pkg_resources as pres
+
+
+def reload_server():
+    os.utime(pres.resource_filename('prototype', 'main.py'), None)
 
 
 def main():
@@ -9,7 +14,7 @@ def main():
 
     application = get_wsgi_application()
     server = Server(application)
-    server.watch('prototype.yml')
+    server.watch('prototype.yml', reload_server)
     server.watch('templates/')
     server.watch('styles/', shell(['sassc', 'styles/main.scss', 'static/css/main.css']))
     server.serve(port=8000)
